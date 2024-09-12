c2cf4288fa356ddbdda2b9f15f0e734a7a4e7b90
Stanleyoes <stanleyokwadwo27@gmail.com>
2024-09-12 13:26:01 +0000

    homework_1

---
+++ Operators_hw.py
@@ -0,0 +1,15 @@
+name = 'Stanley'
+sport = 'Basketball'
+food = 'chicken'
+
+print (food)
+print (name)
+print(sport)
+
+x = int(input('Enter number:'))
+
+if x < 10:
+    print ('That is sad')
+
+if x >= 10:
+    print ('greattt')
